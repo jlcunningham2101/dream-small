52c66b6924e6072dd00576eb578d291d54ae875b
marikadicarlo <mdicarlo19@yahoo.com>
2022-04-27 22:32:04 -0400

    installed apollo server and created schemas

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 
 const { typeDefs, resolvers } = require("./schemas");
-const { authMiddleware } = require("./utils/auth");
-const db = require("./config/connection");
+// const { authMiddleware } = require("./utils/auth");
+const db = require("./server/config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -35,6 +35,7 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:3000/dream-small",
