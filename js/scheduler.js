90aefc5711efdc7c43aa10f3f65e8d32051e345f
danielle-gan <danielle.gan@uconn.edu>
2022-04-28 19:02:54 -0400

    functioning scheduler

--- js/scheduler.js
+++ js/scheduler.js
@@ -4,3 +4,27 @@ function displayDate() {
     $("#currentDay").html(moment(date).format('MMMM Do YYYY, h:mm a'));
 }
 setInterval(displayDate(), 60*1000);
+
+
+//create and load local storage for tasks
+$("#7 .task").val(localStorage.getItem("7"));
+$("#8 .task").val(localStorage.getItem("8"));
+$("#9 .task").val(localStorage.getItem("9"));
+$("#10 .task").val(localStorage.getItem("10"));
+$("#11 .task").val(localStorage.getItem("11"));
+$("#12 .task").val(localStorage.getItem("12"));
+$("#1 .task").val(localStorage.getItem("1"));
+$("#2 .task").val(localStorage.getItem("2"));
+$("#3 .task").val(localStorage.getItem("3"));
+$("#4 .task").val(localStorage.getItem("4"));
+$("#5 .task").val(localStorage.getItem("5"));
+$("#6 .task").val(localStorage.getItem("6"));
+
+//save task
+$(".saveBtn").on("click", function () {
+    var task = $(this).siblings(".task").val();
+    var time = $(this).parent().attr("id");
+
+    //set items in local storage.
+    localStorage.setItem(time, task);
+});
