8e569bfd73bd18bb6afcda009c8e86187525a2e7
danielle-gan <danielle.gan@uconn.edu>
2022-04-28 21:18:57 -0400

    schemas built out

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,74 @@
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return "Hello world!";
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('schedule')
+
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
     },
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('schedule')
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('schedule')
+    },
+    schedules: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Schedule.find(params).sort({ createdAt: -1 });
+    },
+    schedule: async (parent, { _id }) => {
+      return Schedule.findOne({ _id });
+    }
   },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addSchedule: async (parent, args, context) => {
+      if (context.user) {
+        const schedule = await Schedule.create({ ...args, username: context.user.username });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { schedules: schedule._id } },
+          { new: true }
+        );
+
+        return schedule;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  }
 };
 
 module.exports = resolvers;
